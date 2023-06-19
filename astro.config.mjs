236ff0a1062c7017a8772c7b8b3c4fa60c09fd6a
Vadim <angryvadik@gmail.com>
2023-06-19 17:56:03 +0200

    feat: i18n setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
 import { defineConfig } from "astro/config";
 
 import solidJs from "@astrojs/solid-js";
+import i18n from "astro-i18n-aut";
+
+const defaultLocale = "ru";
+const locales = {
+  en: "en-US", // the `defaultLocale` value must present in `locales` keys
+  ru: "ru-RU",
+};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  integrations: [
+    solidJs(),
+    i18n({
+      locales,
+      defaultLocale,
+    }),
+  ],
+  trailingSlash: "always",
 });
