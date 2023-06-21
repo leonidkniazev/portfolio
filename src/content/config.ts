f05def45b20889fc470a78dad723d12feba935c8
Vadim <angryvadik@gmail.com>
2023-06-21 10:50:16 +0200

    feat: content

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,49 @@
 import { defineCollection, z } from "astro:content";
 
-const interest = z.object({
-  aboutMe: z.string(),
-  projects: z.string(),
-  skills: z.string(),
-  contacts: z.string(),
-  resume: z.string(),
+const roundedSkill = z.object({
+  name: z.string(),
+  percent: z.number().min(0).max(100),
+  img: z.string().optional(),
 });
-const interests = defineCollection({
+const framework = z.object({
+  name: z.string(),
+  list: z.string().array().optional(),
+});
+const interest = z.object({
+  name: z.string(),
+  img: z.string(),
+});
+const certificate = z.object({
+  img: z.string(),
+  href: z.string(),
+});
+const project = z.object({
+  img: z.string(),
+  href: z.string(),
+  name: z.string(),
+  description: z.string(),
+});
+
+const portfolio = defineCollection({
   type: "data",
   schema: z.object({
-    interest,
+    hardSkills: z.object({
+      languages: roundedSkill.array(),
+      programmingLanguages: roundedSkill.array(),
+      softwareSkills: roundedSkill.array(),
+      UXResearchSkills: z.string().array(),
+      UXDesignSkills: z.string().array(),
+      UIDesignSkills: z.string().array(),
+      productManagmentSkills: z.string().array(),
+      frameworkSkills: framework.array(),
+    }),
+    softSkills: z.string().array(),
+    certificates: certificate.array(),
+    projects: project.array(),
+    interests: interest.array(),
   }),
 });
 
 export const collections = {
-  translations: interests,
+  portfolio: portfolio,
 };
