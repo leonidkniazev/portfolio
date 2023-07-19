81d7c7c498da42d83c66d86e06e8519ca70f3573
Vadim <angryvadik@gmail.com>
2023-07-19 10:42:14 +0200

    feat: add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
-
 import solidJs from "@astrojs/solid-js";
 import i18n from "astro-i18n-aut";
-
+import tailwind from "@astrojs/tailwind";
 const defaultLocale = "en";
 const locales = {
   en: "en-US",
@@ -17,6 +16,7 @@ export default defineConfig({
       locales,
       defaultLocale,
     }),
+    tailwind({ applyBaseStyles: false }),
   ],
   experimental: {
     assets: true,
