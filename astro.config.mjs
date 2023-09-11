32c00c1d2f1bab3f6a9af03a5faa7983afe317c7
Vadim <angryvadik@gmail.com>
2023-09-11 03:28:01 +0200

    feat: astro 3 migration and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
-import i18n from "astro-i18n-aut";
+import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
 import mdx from "@astrojs/mdx";
 import { locales, defaultLang } from "./src/i18n/ui";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "~": "/src",
+      },
+    },
+  },
   integrations: [
     solidJs(),
     i18n({
@@ -13,9 +21,14 @@ export default defineConfig({
       defaultLocale: defaultLang,
     }),
     mdx(),
+    sitemap({
+      i18n: {
+        locales,
+        defaultLocale: defaultLang,
+      },
+      filter: filterSitemapByDefaultLocale({ defaultLocale: defaultLang }),
+    }),
   ],
-  experimental: {
-    assets: true,
-  },
   trailingSlash: "always",
+  site: "https://www.leonidkniazev.com/",
 });
