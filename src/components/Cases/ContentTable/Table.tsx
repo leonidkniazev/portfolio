52a5505e1a841e2d2860d1b30f2cccb7e7940e01
Vadim <angryvadik@gmail.com>
2023-09-24 07:47:52 +0200

    feat: collapsing chapters

--- src/components/Cases/ContentTable/Table.tsx
+++ src/components/Cases/ContentTable/Table.tsx
@@ -3,22 +3,27 @@ import styles from "./Table.module.css";
 
 type TagType = "H2" | "H3";
 
-type Content = {
-  title: string;
+type Chapter = {
   number: number;
-  topOffset: number;
   href: string;
-  type: TagType;
+  title: string;
+  topOffset: number;
+  children: SubChapter[];
+};
+
+type SubChapter = {
+  title: string;
+  href: string;
 };
 
 export default function Table() {
-  const [currentItem, setCurrentItem] = createSignal(0);
+  const [currentItem, setCurrentItem] = createSignal(1);
   const [isOpen, setIsOpen] = createSignal(true);
   const headings = getHeadings();
   let content = generateContent();
 
   function generateContent() {
-    let result: Content[] = [];
+    let result: Chapter[] = [];
     let number = 0;
     for (const heading of headings) {
       let id = heading.getAttribute("id");
@@ -27,20 +32,29 @@ export default function Table() {
       let offsetFromTop =
         heading.getBoundingClientRect().top +
         window.scrollY -
-        // 30% of screen
+        // 30% of the screen
         (window.innerHeight / 100) * 30;
 
       if (tagname != "H2" && tagname != "H3") {
         continue;
       }
+
       if (id && content) {
-        result.push({
-          href: `#${id}`,
-          topOffset: offsetFromTop,
-          title: content,
-          type: tagname,
-          number: tagname === "H2" ? ++number : number,
-        });
+        if (tagname == "H3" && result[number - 1]) {
+          result[number - 1].children.push({
+            href: `#${id}`,
+            title: content,
+          });
+        }
+        if (tagname == "H2") {
+          result.push({
+            href: `#${id}`,
+            topOffset: offsetFromTop,
+            title: content,
+            number: ++number,
+            children: [],
+          });
+        }
       }
     }
     return result;
@@ -53,14 +67,12 @@ export default function Table() {
   function onScroll() {
     if (!isOpen()) return;
     const currentScroll = window.scrollY;
-    let rightItemNumber = 1;
     content.forEach((item) => {
       if (item.topOffset < currentScroll) {
-        rightItemNumber = item.number;
+        setCurrentItem(item.number);
         return;
       }
     });
-    setCurrentItem(rightItemNumber);
   }
 
   function onResize() {
@@ -88,31 +100,35 @@ export default function Table() {
       {isOpen() && (
         <div class={styles.container}>
           <ol class={styles.list}>
-            {content.map((item) => {
-              return (
+            {content.map((chapter) => (
+              <>
                 <li
-                  class={`${item.type == "H2" ? styles.large : styles.small} ${
-                    item.number == currentItem() && item.type != "H3"
-                      ? styles.active
-                      : ""
+                  class={`${styles.large} ${
+                    chapter.number == currentItem() ? styles.active : ""
                   }`}
                 >
-                  <a
-                    class={`${styles.link} ${
-                      item.type == "H3" && styles.subtitle
-                    }`}
-                    href={item.href}
-                  >
-                    {item.type == "H2" ? (
-                      <span>{item.number}. </span>
-                    ) : (
-                      <span>- </span>
-                    )}
-                    {item.title}
+                  <a class={styles.link} href={chapter.href}>
+                    <span>{chapter.number}. </span>
+                    {chapter.title}
                   </a>
                 </li>
-              );
-            })}
+                <div
+                  class={`${styles.childrenContainer} ${
+                    chapter.number == currentItem() ? styles.open : ""
+                  }`}
+                >
+                  <ul>
+                    {chapter.children.map((child) => (
+                      <li class={styles.small}>
+                        <a href={child.href}>
+                          <span>{child.title}</span>
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </>
+            ))}
           </ol>
         </div>
       )}
